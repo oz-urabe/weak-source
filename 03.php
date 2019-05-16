223c70eff15169eaad5982122049f36f4e01ea1a
Shinichi Urabe <s-urabe@oz-vision.co.jp>
2019-05-16 14:50:48 +0900

    DOM-based XSS

---
+++ 03.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>02</title>
+<!--CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+<!--JS -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+    value: <span id="write"></span>
+<script>
+    $("#write").html(decodeURIComponent(location.hash.substring(1)));
+</script>
+</body>
+</html>
